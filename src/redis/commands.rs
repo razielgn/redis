fe85d1a485287efb99920b6957f7b44680ed43d0
Federico Ravasio <ravasio.federico@gmail.com>
2016-05-24 23:38:38 +0200

    Sorted commands enum.

--- src/redis/commands.rs
+++ src/redis/commands.rs
@@ -2,13 +2,13 @@ pub type Bytes<'a> = &'a [u8];
 
 #[derive(PartialEq, Eq, Debug, Clone)]
 pub enum Command<'a> {
-    Set { key: Bytes<'a>, value: Bytes<'a> },
-    Get { key: Bytes<'a> },
-    Exists { keys: Vec<Bytes<'a>> },
-    Del { keys: Vec<Bytes<'a>> },
-    Rename { key: Bytes<'a>, new_key: Bytes<'a> },
-    IncrBy { key: Bytes<'a>, by: i64 },
-    DecrBy { key: Bytes<'a>, by: i64 },
-    Strlen { key: Bytes<'a> },
     Append { key: Bytes<'a>, value: Bytes<'a> },
+    DecrBy { key: Bytes<'a>, by: i64 },
+    Del { keys: Vec<Bytes<'a>> },
+    Exists { keys: Vec<Bytes<'a>> },
+    Get { key: Bytes<'a> },
+    IncrBy { key: Bytes<'a>, by: i64 },
+    Rename { key: Bytes<'a>, new_key: Bytes<'a> },
+    Set { key: Bytes<'a>, value: Bytes<'a> },
+    Strlen { key: Bytes<'a> },
 }
